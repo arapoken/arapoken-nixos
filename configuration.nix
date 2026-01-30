1e1c22f8d6cef12bc3195e94c1b9a6a29c7568a9
arapoken <821076631@qq.com>
2026-01-31 02:14:27 +0800

    Add initial NixOS configuration
    This configuration file sets up system packages, services, and user accounts for NixOS.

---
+++ configuration.nix
@@ -0,0 +1,180 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 5;
+  };
+  boot.loader.timeout = null;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable networking
+  networking = {
+    networkmanager.enable = true;
+    hostName = "kitty"; # Define your hostname
+  };
+
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  hardware.bluetooth.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
+  services.udisks2.enable = true;
+
+  # Set your time zone
+  time.timeZone = "Asia/Shanghai";
+
+  # Select internationalisation properties
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure inputMehtod
+  i18n.inputMethod = {
+    type = "fcitx5";
+    enable = true;
+    fcitx5.addons = with pkgs; [
+      qt6Packages.fcitx5-chinese-addons
+      qt6Packages.fcitx5-qt
+      fcitx5-gtk
+    ];
+    fcitx5.waylandFrontend = true;
+  };
+
+  # Configure fonts
+  fonts.packages = with pkgs; [
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    nerd-fonts.roboto-mono
+  ];
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  # Configure login manager  
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri";
+        user = "greeter";
+      };
+    };
+  };
+
+  # Define a user account
+  users.users.arapoken = {
+    isNormalUser = true;
+    description = "arapoken";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    useDefaultShell = true;
+  };
+  users.defaultUserShell = pkgs.fish;
+
+  # Configure home-manager
+  home-manager = {
+    useGlobalPkgs = true;
+    backupFileExtension = "backup";
+    users.arapoken = import ./home.nix;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  nixpkgs.overlays = [
+    (self: super: {
+      noctalia-shell = (import <nixos-unstable> { 
+        system = config.nixpkgs.system; 
+        config.allowUnfree = true;
+      }).noctalia-shell;
+    })
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    btop
+    clash-verge-rev
+    fastfetch
+    # fuzzel
+    git
+    kitty
+    neovim
+    # vim
+    # wget 
+    xwayland-satellite
+    yazi
+    noctalia-shell
+  ];
+
+  programs = {
+    firefox.enable = true;
+    fish.enable = true;
+    niri.enable = true;
+  };
+
+  nix.settings = {
+    substituters = [
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
